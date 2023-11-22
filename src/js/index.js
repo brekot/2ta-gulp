05e3b897a8a0465b705da840f41fbbfc44dc4864
Andrey Brekotkin <brekot@ya.ru>
2023-11-22 19:54:30 +0300

    Все страницы

--- src/js/index.js
+++ src/js/index.js
@@ -35,8 +35,47 @@ document.addEventListener('click', (e) => {
             behavior : "smooth"
         });
     }
+
+    // Блок поделиться
+    let share_elem = e.target.closest('.share-block__open'),
+        share_block = document.querySelector('.share-block-panel');
+
+    if (share_elem)
+    {
+        share_block.classList.toggle('share-block-panel_active')
+    }
+    else if (share_block && share_block.classList.contains('share-block-panel_active'))
+    {
+        share_block.classList.remove('share-block-panel_active')
+    }
+
+    // Кнопка Копировать ссылку
+    if (e.target.closest('.share-block-panel__link_copy'))
+    {
+		let input = document.createElement('textarea');
+
+		input.style.fontSize = '12pt';
+		input.style.border = '0';
+		input.style.padding = '0';
+		input.style.margin = '0';
+		input.style.position = 'absolute';
+		input.style.left = '-9999px';
+		input.setAttribute('readonly', '');
+
+		input.value = window.location.href;
+
+		document.body.appendChild(input);
+
+		input.select();
+		input.setSelectionRange(0, 99999);
+
+		document.execCommand('copy');
+
+		input.remove();
+    }
 });
 
+// Показать кнопку наверх
 document.addEventListener('scroll', () => {
 
     if (pageYOffset > 200)
